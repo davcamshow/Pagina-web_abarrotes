1e8fe9226f998ca889c902311914ca9822885faf
davcamshow <davidbarrerareyes98@gmail.com>
2025-09-27 15:29:12 -0600

    Agregar sistema de login y conexión a base de datos

---
+++ DB_Conection/Conexion_BD.php
@@ -0,0 +1,12 @@
+
+<?php include "../Html/Sobre nosotros.html"; 
+$servidor ="localhost";
+$usuario ="root";
+$clave ="";
+$baseDeDatos ="abarrotes_bd";
+
+
+$enlace=mysqli_connect ($servidor, $usuario, $clave, $baseDeDatos);
+
+?>
+
